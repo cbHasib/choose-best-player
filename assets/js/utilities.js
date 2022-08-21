c4e018e33d99d34e1dd6dac2aa8adcf82e3be443
cbHasib <hasibul.pbn@gmail.com>
2022-08-22 03:21:41 +0600

    Function Parameter correction

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -1,43 +1,39 @@
 function playerSelect(element) {
-    const playerList = document.getElementById("selected-player-list");
-  
-    const numberOfPlayer = numberOfSelectedPlayer();
-    if (numberOfPlayer >= 5) {
-      window.location.href = "#selectWarning";
-      return;
-    }
-  
-    const playerName = element.parentNode.parentNode.children[0].innerText;
-    const li = document.createElement("li");
-    li.innerText = playerName;
-    playerList.appendChild(li);
-  
-    element.setAttribute("disabled", "disabled");
-  }
-  
-  
-  function numberOfSelectedPlayer(){
-      const playerList = document.getElementById("selected-player-list");
-      const playerNumber = playerList.children.length;
-      return playerNumber
+  const playerList = document.getElementById("selected-player-list");
+
+  const numberOfPlayer = numberOfSelectedPlayer();
+  if (numberOfPlayer >= 5) {
+    window.location.href = "#selectWarning";
+    return;
   }
 
+  const playerName = element.parentNode.parentNode.children[0].innerText;
+  const li = document.createElement("li");
+  li.innerText = playerName;
+  playerList.appendChild(li);
 
-  function getInputValue(elementId){
-    const element = document.getElementById(elementId);
-    const elementValue = parseFloat(element.value);
-    return elementValue;
-  }
+  element.setAttribute("disabled", "disabled");
+}
 
+function numberOfSelectedPlayer() {
+  const playerList = document.getElementById("selected-player-list");
+  const playerNumber = playerList.children.length;
+  return playerNumber;
+}
 
-  function setElementValue(elementId, value){
-    const element = document.getElementById(elementId);
-    element.innerText = value.toFixed(2);
-  }
+function getInputValue(elementId) {
+  const element = document.getElementById(elementId);
+  const elementValue = parseFloat(element.value);
+  return elementValue;
+}
 
+function setElementValue(elementId, value) {
+  const element = document.getElementById(elementId);
+  element.innerText = value.toFixed(2);
+}
 
-  function getElementValue(elementId, value){
-    const element = document.getElementById(elementId);
-    const elementValue = parseFloat(element.innerText);
-    return elementValue;
-  }
+function getElementValue(elementId) {
+  const element = document.getElementById(elementId);
+  const elementValue = parseFloat(element.innerText);
+  return elementValue;
+}
